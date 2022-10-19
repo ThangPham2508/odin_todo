2d2142180cf455c2b25f87c349e153141992f6d5
Thang <phamducthang2508@gmail.com>
2022-10-20 10:08:46 +1200

    Change README

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # odin_todo
-Todo list.
+
+A simple todo list (storing data unsupported).
+
+Note: Click on the title to change the todo's title and make sure to press Enter when you're done.
--- src/index.js
+++ src/index.js
@@ -1,53 +1,58 @@
 import pageLoad from './Modules/pageLoad.js';
-import {DOMManip} from './Modules/DOM.js';
-import {list, projectList} from './Modules/todo-list.js';
-import {display} from './Modules/display.js';
+import { DOMManip } from './Modules/DOM.js';
+import { list, projectList } from './Modules/todo-list.js';
+import { display } from './Modules/display.js';
 import './style.css';
+let ghpages = require('gh-pages');
 
 pageLoad();
 
 const addtask = document.querySelector('.addtask');
 addtask.addEventListener('click', () => {
-    let todo = list.addTodo()
-    DOMManip.createTodo(todo);
-    projectList.addTodo(todo);
+  const todo = list.addTodo();
+  DOMManip.createTodo(todo);
+  projectList.addTodo(todo);
 });
 
 const todolist = document.querySelector('.todolist');
 const nav = document.querySelectorAll('.categories button');
 
 nav.forEach((item) => {
-    item.addEventListener('click', (e) => {
-        let target = e.target;
-        while (target.tagName !== 'BUTTON') {
-            target = target.parentElement;
-        }
-        todolist.textContent = '';
-        for (let item of nav) {
-            item.classList.remove('selected');
-        }
-        const projectNav = document.querySelectorAll('.projectList .project');
-        for (let item of projectNav) {
-            item.classList.remove('selected');
-        }
-        target.classList.add('selected');
-        if (target.classList.contains('alltasks')) {
-            display.displayAll();
-        } 
-        else if (target.classList.contains('today')) {
-            display.displayToday();
-        }
-        else {
-            display.displayImportant();
-        }
-        projectList.setActive(null);
-        console.log(target.classList[0]);
-    }, {
-        capture: false
-    });
+  item.addEventListener(
+    'click',
+    (e) => {
+      let { target } = e;
+      while (target.tagName !== 'BUTTON') {
+        target = target.parentElement;
+      }
+      todolist.textContent = '';
+      for (const item of nav) {
+        item.classList.remove('selected');
+      }
+      const projectNav = document.querySelectorAll('.projectList .project');
+      for (const item of projectNav) {
+        item.classList.remove('selected');
+      }
+      target.classList.add('selected');
+      if (target.classList.contains('alltasks')) {
+        display.displayAll();
+      } else if (target.classList.contains('today')) {
+        display.displayToday();
+      } else {
+        display.displayImportant();
+      }
+      projectList.setActive(null);
+      console.log(target.classList[0]);
+    },
+    {
+      capture: false,
+    },
+  );
 });
 
 const project = document.querySelector('.addproject');
 project.addEventListener('click', () => {
-    DOMManip.promptProject();
-})
+  DOMManip.promptProject();
+});
+
+ghpages.publish('dist', function(err) {});
